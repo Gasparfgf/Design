676e6d2c84c726c4054af6e438f9790e6d6fb190
gaspardarosafrancisco@gmail.com <gaspar-da-rosa.francisco@etu.univ-usmb.fr>
2025-03-08 14:52:37 +0100

    add isp exercice

---
+++ SOLID/isp/RobotWorker.java
@@ -0,0 +1,9 @@
+class RobotWorker implements Worker {
+    public void work() {
+        System.out.println("Le robot travaille !");
+    }
+
+    public void eat() {
+        throw new UnsupportedOperationException("Un robot ne mange pas !");
+    }
+}

d32f5b34ebde2361a9865c5c7a8ee83920ec1bbd
gaspardarosafrancisco@gmail.com <gaspar-da-rosa.francisco@etu.univ-usmb.fr>
2025-03-08 20:12:56 +0100

    feat: set java project

--- SOLID/dip/Application.java
+++
@@ -1,6 +0,0 @@
-/**
- * Le code ci-dessous dépend directement d’une base de données MySQL.
- * Modifie-le pour qu'il respecte le DIP en utilisant une interface.
- *
- * ✅ Objectif : Injecte une dépendance au lieu de dépendre directement de MySQLDatabase.
- * */
--- SOLID/isp/RobotWorker.java
+++
@@ -1,9 +0,0 @@
-class RobotWorker implements Worker {
-    public void work() {
-        System.out.println("Le robot travaille !");
-    }
-
-    public void eat() {
-        throw new UnsupportedOperationException("Un robot ne mange pas !");
-    }
-}
